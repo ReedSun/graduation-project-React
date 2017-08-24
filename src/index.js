d8d0f129584d63aa075e346ba165ebea270889e5
ReedSun <superreedsun@gmail.com>
2017-08-24 21:45:16 +0800

    add mission2

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import headerImg from './balloon.png'
+import './hello_world.css';
 
 ReactDOM.render(
-  <h1>Hi, world!</h1>,
-  document.getElementById('root2')
+  <div className="main">
+    <img src={headerImg} className="header" alt="balloon" />
+    <h1 className="title">Hello, world!</h1>
+    <p className="desp">Any fool can write code that a computer can understand. Good programmers write code that humans can understand.</p>
+  </div>,
+  document.getElementById('root')
 )
